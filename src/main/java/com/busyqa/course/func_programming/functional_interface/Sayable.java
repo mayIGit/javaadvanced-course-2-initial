02fed22f5e8291264ce167e92935698f811cb770
Carlos Zegarra <zegarra@uwindsor.ca>
2019-11-15 15:24:03 -0500

    first commit

---
+++ src/main/java/com/busyqa/course/func_programming/functional_interface/Sayable.java
@@ -0,0 +1,6 @@
+package com.busyqa.course.func_programming.functional_interface;
+
+@FunctionalInterface
+public interface Sayable{  
+    void say(String msg);  
+}  
---
+++ src/main/java/com/busyqa/course/func_programming/lambda_expression/Sayable.java
@@ -0,0 +1,6 @@
+package com.busyqa.course.func_programming.lambda_expression;
+
+@FunctionalInterface
+public interface Sayable {
+	public String say();
+}
